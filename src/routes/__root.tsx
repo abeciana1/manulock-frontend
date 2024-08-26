27470a6342561bc98eb7ec6cb8f57dbcbe7a5c30
Alex <alex.beciana@gmail.com>
2024-08-25 20:11:31 -0600

    Updated component name

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,10 +3,10 @@ import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 export const Route = createRootRoute({
-  component: () => RootAppSignin,
+  component: () => RootAppSignIn,
 });
 
-const RootAppSignin = () => {
+const RootAppSignIn = () => {
   return (
     <>
       <div className="p-2 flex gap-2">
