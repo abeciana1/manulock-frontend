4e3ff3a42c5fd17cfaff94cced8e2b492ff1d026
Alex <alex.beciana@gmail.com>
2024-08-25 19:56:12 -0600

    Added configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
-// https://vitejs.dev/config/
+const isTest = process.env.NODE_ENV === 'test';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [!isTest && TanStackRouterVite(), react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './test/setup.js',
+  },
+});
