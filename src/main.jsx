f85394ccdfaf26868a9b999f8a713202f8c1701d
Alex <alex.beciana@gmail.com>
2024-08-25 20:42:52 -0600

    Added ClerkProvider wrapper

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,9 @@ import './index.css';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ClerkProvider } from '@clerk/clerk-react'
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 const queryClient = new QueryClient();
 
 const router = createRouter({
@@ -19,10 +21,12 @@ const router = createRouter({
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router}>
-        <App />
-      </RouterProvider>
-    </QueryClientProvider>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router}>
+          <App />
+        </RouterProvider>
+      </QueryClientProvider>
+    </ClerkProvider>
   </StrictMode>
 );
