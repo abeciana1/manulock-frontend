b44750d2bf7a656ab95515070efe3d38a496b9ae
Alex <alex.beciana@gmail.com>
2024-08-26 21:00:56 -0600

    Created component for login

--- src/components/SignIn/index.jsx
+++ src/components/SignIn/index.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
-import { SignedOut, SignInButton } from "@clerk/clerk-react";
+import { SignedOut, SignInButton, useSignIn } from "@clerk/clerk-react";
+import { Heading1 } from '../_styled/Headings'
+import { SolidButton } from '../_inputs/Buttons'
 
 const SignIn = () => {
+    const { signIn } = useSignIn()
+
+    console.log('res sign in', signIn)
+
     return (
-        <>
-            <SignedOut>
-                <SignInButton/>
-            </SignedOut>
-        </>
+        <section className='hero w-full max-w-xs sm:max-w-md mx-auto flex justify-center items-center h-screen'>
+            <div className="hero-content bg-accent-yellow text-center flex flex-col rounded-md">
+                <Heading1 text='Sign in or sign up for an account' color='primary' />
+                <div className="py-6">Click on the button below to sign into your account or create a new one.</div>
+                <SignedOut>
+                    <SignInButton mode='modal'>
+                        <SolidButton
+                            text='Sign in/Sign up'
+                            color='blue'
+                        />
+                    </SignInButton>
+                </SignedOut>
+            </div>
+        </section>
     )
 }
 
