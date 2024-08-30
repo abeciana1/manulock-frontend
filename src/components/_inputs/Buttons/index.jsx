7cd1c6a97cd7ff34a9f5b404415179de7077d3cf
Alex <alex.beciana@gmail.com>
2024-08-29 21:01:49 -0600

    Added prop types and creation of SolidButton component

--- src/components/_inputs/Buttons/index.jsx
+++ src/components/_inputs/Buttons/index.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
-import cx from 'classnames'
+import React from 'react';
+import cx from 'classnames';
+import PropTypes from 'prop-types';
 
-export const SolidButton = ({
-    text,
-    color,
-    onClick = () => {},
-    icon
-}) => {
-    return (
-        <button
-            onClick={onClick}
-            className={cx('text-lg rounded-mg py-1 px-2', {
-                ['bg-accent-blue text-primary-light']: color === 'blue',
-                ['bg-accent-yellow text-primary']: color === 'yellow',
-                ['bg-accent-red text-primary-light']: color === 'red',
-                ['bg-accent-green text-primary']: color === 'green'
-            })}
-        >
-            { text }
-        </button>
-    )
-}
+export const SolidButton = ({ text, color, onClick, icon }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={cx('text-lg rounded-lg py-1 px-2', {
+        ['bg-accent-blue text-primary-light']: color === 'blue',
+        ['bg-accent-yellow text-primary']: color === 'yellow',
+        ['bg-accent-red text-primary-light']: color === 'red',
+        ['bg-accent-green text-primary']: color === 'green',
+      })}
+    >
+      {text}
+    </button>
+  );
+};
+
+SolidButton.propTypes = {
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  icon: PropTypes.element,
+};
