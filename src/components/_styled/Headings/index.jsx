436f2365b505c58579176ed02b033c1b884894c8
Alex <alex.beciana@gmail.com>
2024-08-29 21:03:09 -0600

    Created Heading1 component

--- src/components/_styled/Headings/index.jsx
+++ src/components/_styled/Headings/index.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import cx from 'classnames'
+import React from 'react';
+import cx from 'classnames';
 
 export const Heading1 = ({ text, color = 'primary' }) => {
-    return (
-        <h1
-            className={cx('text-5xl font-bold', {
-                ['text-primary']: color === 'primary',
-                ['text-primary-light']: color === 'light',
-                ['text-accent-red']: color === 'red',
-                ['text-accent-yellow']: color === 'yellow',
-                ['text-accent-green']: color === 'green',
-                ['text-accent-blue']: color === 'blue'
-            })}
-        >{ text }</h1>
-    )
-}
+  return (
+    <h1
+      className={cx('text-5xl font-bold', {
+        ['text-primary']: color === 'primary',
+        ['text-primary-light']: color === 'light',
+        ['text-accent-red']: color === 'red',
+        ['text-accent-yellow']: color === 'yellow',
+        ['text-accent-green']: color === 'green',
+        ['text-accent-blue']: color === 'blue',
+      })}
+    >
+      {text}
+    </h1>
+  );
+};
