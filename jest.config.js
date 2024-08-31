3307f985238d2422e7ba35fc5c51a060ced285e5
Alex <alex.beciana@gmail.com>
2024-08-31 11:53:20 -0600

    Changed config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+export default {
+  testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
 };
