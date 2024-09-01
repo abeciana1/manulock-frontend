27fe588fc91b0425b10a1d7ca0af20821e6d7afc
Alex <alex.beciana@gmail.com>
2024-09-01 08:43:18 -0600

    Replaced import with work react-query module

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen'; // Ensure this is correct
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { ClerkProvider } from '@clerk/clerk-react';
 
 // Retrieve the Clerk publishable key from the environment variables
