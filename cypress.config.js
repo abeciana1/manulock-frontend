21a578582e414406983ed7d7cd697b0a02313627
Alex <alex.beciana@gmail.com>
2024-09-22 20:24:44 -0600

    Added base url and clerk auth setup

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'cypress';
+import { clerkSetup } from '@clerk/testing/cypress';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
+  env: {
+    CLERK_TESTING_TOKEN: process.env.VITE_CLERK_TESTING_TOKEN,
+  },
   e2e: {
+    baseUrl: 'http://localhost:5173',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      return clerkSetup({ config });
     },
   },
 
