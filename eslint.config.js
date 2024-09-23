6fe4503da250ac866c72e9d3501d4c1ac3fe6594
Alex <alex.beciana@gmail.com>
2024-09-22 20:25:31 -0600

    Added cypress plugin

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import pluginJs from '@eslint/js';
 import pluginReact from 'eslint-plugin-react';
 import prettier from 'eslint-config-prettier';
 import pluginPrettier from 'eslint-plugin-prettier';
+import pluginCypress from 'eslint-plugin-cypress';
 
 export default [
   { files: ['**/*.{js,mjs,cjs,jsx,tsx}'] }, // Removed .ts extension
@@ -12,6 +13,7 @@ export default [
         ...globals.browser,
         ...globals.node,
         ...globals.jest,
+        ...globals.cypress,
       },
     },
   },
@@ -21,10 +23,21 @@ export default [
   {
     plugins: {
       prettier: pluginPrettier,
+      cypress: pluginCypress,
     },
     rules: {
       'prettier/prettier': 'error',
       'react/react-in-jsx-scope': 'off',
+      'cypress/no-unnecessary-waiting': 'warn',
+      'cypress/no-assigning-return-values': 'error',
+    },
+  },
+  {
+    files: ['cypress/**/*.cy.{js,jsx,ts,tsx}'],  // Target Cypress test files specifically
+    languageOptions: {
+      globals: {
+        ...globals.cypress,  // Add Cypress globals like 'cy' and 'Cypress'
+      },
     },
   },
 ];
