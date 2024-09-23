8448d3ba80b450fdc2c95a89b220aff2b2449967
Alex <alex.beciana@gmail.com>
2024-09-22 20:23:09 -0600

    Added clerk commands to cypress

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,20 +1,6 @@
-// ***********************************************************
-// This example support/e2e.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
+/// <reference types="cypress" />
+import { addClerkCommands } from '@clerk/testing/cypress';
+import Cypress from 'cypress';
+import cy from 'cypress';
 
-// Import commands.js using ES2015 syntax:
-import './commands';
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+addClerkCommands({ Cypress, cy });
