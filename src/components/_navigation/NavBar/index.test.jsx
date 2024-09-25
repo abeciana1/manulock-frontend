f1a4e6e93f942072cbd37a1be3f8b56dce7d0dda
Alex <alex.beciana@gmail.com>
2024-09-24 20:28:01 -0600

    Created test for NavBar component

---
+++ src/components/_navigation/NavBar/index.test.jsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import NavBar from '../NavBar';
+
+describe('NavBar', () => {
+  beforeEach(() => {
+    render(<NavBar />);
+  });
+  test('should render with role of nav', () => {
+    const navBar = screen.getByRole('navigation');
+    expect(navBar).toBeInTheDocument();
+  });
+});
