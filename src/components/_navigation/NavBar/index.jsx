dfb7592faef1e28e548434277109f7b292cd4592
Alex <alex.beciana@gmail.com>
2024-09-29 10:37:50 -0600

    Added navigation logo

--- src/components/_navigation/NavBar/index.jsx
+++ src/components/_navigation/NavBar/index.jsx
@@ -1,5 +1,20 @@
+import Image from '../../_styled/Image';
+
 const NavBar = () => {
-  return <nav></nav>;
+  return (
+    <nav className="px-10 py-3 bg-white">
+      <div>
+        <Image
+          src="https://media.graphassets.com/output=format:jpg/BZcUI4wrSx6BB1zpOyO6"
+          alt="Manulock logo"
+          height={620}
+          width={1060}
+          priority
+          className="h-10 w-20"
+        />
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
