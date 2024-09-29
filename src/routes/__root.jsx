86a1329702c78712ffb56d81348eb55f0089643c
Alex <alex.beciana@gmail.com>
2024-09-29 10:36:20 -0600

    Adjusted logic for unauthed user

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -1,14 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { createRootRoute, useNavigate, Outlet } from '@tanstack/react-router';
 import { useSession } from '@clerk/clerk-react';
 import { useMutation } from 'react-query';
 import useAxiosInstance from '../hooks/useAxiosInstance';
 import NavBar from '../components/_navigation/NavBar';
+import Login from '../components/Login';
 
 const RootAppSignIn = () => {
   const navigate = useNavigate();
   const axiosInstance = useAxiosInstance();
   const { session, isSignedIn } = useSession();
+  const [hasMutated, setHasMutated] = useState(false); // To prevent multiple mutations
   const findOrCreateUser = async (data) => {
     const response = await axiosInstance.post('/users', {
       user: data,
@@ -18,19 +20,26 @@ const RootAppSignIn = () => {
 
   const { mutate } = useMutation(findOrCreateUser, {
     onSuccess: (data) => {
+      console.log('data', data);
       if (data.status === 200) {
         navigate({ to: '/dashboard' });
       }
     },
     onError: () => {
       console.log('error finding or creating user');
-      navigate({ to: '/' });
+      navigate({ to: '/signup' });
     },
   });
 
   useEffect(() => {
-    if (isSignedIn && session?.user) {
-      // Added conditional for URL params - if invited-by-id # -> role = 1
+    // Redirect to /signin if not signed in
+    if (!session?.user) {
+      navigate({ to: '/signin' });
+      return;
+    }
+
+    // Only call mutate if the user is signed in, the session is available, and we haven't mutated yet
+    if (isSignedIn && session?.user && !hasMutated) {
       const userData = {
         first_name: session?.user?.firstName,
         last_name: session?.user?.lastName,
@@ -39,14 +48,14 @@ const RootAppSignIn = () => {
         role: 0,
       };
       mutate(userData);
-      // post request to backend - find or create user
-      // window.Clerk redirect to dashboard page
+      setHasMutated(true); // Ensure mutate is only called once
     }
-  }, [session?.user?.id]);
+  }, [session?.user, hasMutated]);
 
   return (
     <>
       {isSignedIn && <NavBar />}
+      {!isSignedIn && <Login />}
       <Outlet />
     </>
   );
