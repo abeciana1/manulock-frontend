580dee35ac04288a3be01a0659eaf35389ef6111
Alex <alex.beciana@gmail.com>
2024-09-29 17:31:39 -0600

    Commented out image component

--- src/components/_navigation/NavBar/index.jsx
+++ src/components/_navigation/NavBar/index.jsx
@@ -1,9 +1,9 @@
-import Image from '../../_styled/Image';
+import UserAccount from '../UserAccount';
 
 const NavBar = () => {
   return (
-    <nav className="px-10 py-3 bg-white">
-      <div>
+    <nav className="px-10 py-3 flex justify-end items-center">
+      {/* <div>
         <Image
           src="https://media.graphassets.com/output=format:jpg/BZcUI4wrSx6BB1zpOyO6"
           alt="Manulock logo"
@@ -12,6 +12,9 @@ const NavBar = () => {
           priority
           className="h-10 w-20"
         />
+      </div> */}
+      <div>
+        <UserAccount />
       </div>
     </nav>
   );
