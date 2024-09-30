beef1445547f2109cc50ce1ca77520c9f9d9f7ef
Alex <alex.beciana@gmail.com>
2024-09-29 19:37:46 -0600

    Removed console logs and adjusted redirect logic

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -4,13 +4,12 @@ import { useSession } from '@clerk/clerk-react';
 import { useMutation } from 'react-query';
 import useAxiosInstance from '../hooks/useAxiosInstance';
 import NavBar from '../components/_navigation/NavBar';
-import Login from '../components/Login';
 
 const RootAppSignIn = () => {
   const navigate = useNavigate();
   const axiosInstance = useAxiosInstance();
   const { session, isSignedIn } = useSession();
-  const [hasMutated, setHasMutated] = useState(false); // To prevent multiple mutations
+  const [hasMutated, setHasMutated] = useState(false);
   const findOrCreateUser = async (data) => {
     const response = await axiosInstance.post('/users', {
       user: data,
@@ -20,25 +19,24 @@ const RootAppSignIn = () => {
 
   const { mutate } = useMutation(findOrCreateUser, {
     onSuccess: (data) => {
-      console.log('data', data);
       if (data.status === 200) {
         navigate({ to: '/dashboard' });
       }
     },
     onError: () => {
       console.log('error finding or creating user');
-      navigate({ to: '/' });
+      navigate({ to: '/signin' });
     },
   });
 
   useEffect(() => {
     // Redirect to /signin if not signed in
-    if (!session?.user) {
+    if (!isSignedIn) {
       navigate({ to: '/signin' });
       return;
     }
 
-    // Only call mutate if the user is signed in, the session is available, and we haven't mutated yet
+    // Prevent mutation and redirection during sign-out process
     if (isSignedIn && session?.user && !hasMutated) {
       const userData = {
         first_name: session?.user?.firstName,
@@ -50,12 +48,11 @@ const RootAppSignIn = () => {
       mutate(userData);
       setHasMutated(true); // Ensure mutate is only called once
     }
-  }, [session?.user, hasMutated]);
+  }, [isSignedIn, session?.user, hasMutated, mutate, navigate]);
 
   return (
     <>
       {isSignedIn && <NavBar />}
-      {!isSignedIn && <Login />}
       <Outlet />
     </>
   );
