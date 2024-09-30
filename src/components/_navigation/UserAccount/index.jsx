1bc1f5a10ff280b245d3085bd627b1b7e8faf023
Alex <alex.beciana@gmail.com>
2024-09-29 18:10:23 -0600

    Separated out menu options

--- src/components/_navigation/UserAccount/index.jsx
+++ src/components/_navigation/UserAccount/index.jsx
@@ -3,6 +3,7 @@ import Image from '../../_styled/Image';
 import { HiMiniChevronDown, HiMiniChevronUp } from 'react-icons/hi2';
 import { SolidButton } from '../../_inputs/Buttons';
 import { PiSignOutBold } from 'react-icons/pi';
+import { FaGear } from 'react-icons/fa6';
 
 const UserAccount = () => {
   const [userData, setUserData] = useState(null);
@@ -24,34 +25,44 @@ const UserAccount = () => {
   };
 
   return (
-    <div
-      data-testId="user-account"
-      className="relative ring-neutral-light ring-1 rounded-md py-2 px-2 bg-white w-44 max-w-44"
-    >
-      {userData && (
-        <div className="flex gap-3 justify-between items-center">
-          {userData?.hasImage && (
-            <Image
-              src={userData?.imageUrl}
-              alt={`${userData?.firstName} profile picture`}
-              height={32}
-              width={32}
-              priority
-              className="rounded-md"
+    <>
+      <div
+        data-testId="user-account"
+        className="relative ring-neutral-light ring-1 rounded-md py-2 px-2 bg-white w-44 max-w-44"
+      >
+        {userData && (
+          <div className="flex gap-3 justify-between items-center">
+            {userData?.hasImage && (
+              <Image
+                src={userData?.imageUrl}
+                alt={`${userData?.firstName} profile picture`}
+                height={32}
+                width={32}
+                priority
+                className="rounded-md"
+              />
+            )}
+            <div className="text-lg font-medium">
+              {'Hi, ' + userData?.firstName}
+            </div>
+            <SolidButton
+              onClick={toggleUserMenu}
+              iconFirst
+              icon={menuOpen ? HiMiniChevronUp : HiMiniChevronDown}
             />
-          )}
-          <div className="text-lg font-medium">
-            {'Hi, ' + userData?.firstName}
           </div>
-          <SolidButton
-            onClick={toggleUserMenu}
-            iconFirst
-            icon={menuOpen ? HiMiniChevronUp : HiMiniChevronDown}
-          />
-        </div>
-      )}
+        )}
+      </div>
       {menuOpen && (
-        <ul>
+        <ul className="text-lg relative ring-neutral-light ring-1 rounded-md py-2 px-2 bg-white w-44 max-w-44 mt-2">
+          <a
+            title="User settings"
+            href="https://blessed-peacock-72.accounts.dev/user"
+            className="flex items-center gap-2 py-1 px-2"
+          >
+            <FaGear />
+            <li>Settings</li>
+          </a>
           <li>
             <SolidButton
               iconFirst
@@ -62,7 +73,7 @@ const UserAccount = () => {
           </li>
         </ul>
       )}
-    </div>
+    </>
   );
 };
 
