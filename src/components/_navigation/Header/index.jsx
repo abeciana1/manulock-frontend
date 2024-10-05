a8585f4979a502fc5f126c9e672095dc03ff04b5
Alex <alex.beciana@gmail.com>
2024-10-05 10:13:52 -0600

    Created header component

---
+++ src/components/_navigation/Header/index.jsx
@@ -0,0 +1,11 @@
+import UserAccount from '../UserAccount';
+
+const Header = () => {
+  return (
+    <section className="px-10 py-3 flex justify-end items-center">
+      <UserAccount />
+    </section>
+  );
+};
+
+export default Header;
