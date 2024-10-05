546035609602d179e789ae79cbbf49b8226d8f30
Alex <alex.beciana@gmail.com>
2024-10-05 13:35:49 -0600

    Formatted and fixed issue with auto redirection

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -1,9 +1,33 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router';
+import {
+  createRootRoute,
+  Outlet,
+  useNavigate,
+  useLocation,
+} from '@tanstack/react-router';
 import { useSession } from '@clerk/clerk-react';
 import NavBar from '../components/_navigation/NavBar';
+import { useEffect } from 'react';
 
 const RootAppSignIn = () => {
-  const { isSignedIn } = useSession();
+  const navigate = useNavigate();
+  const { isSignedIn, isLoaded } = useSession();
+  const location = useLocation();
+  console.log('location', location);
+
+  useEffect(() => {
+    if (isLoaded) {
+      if (!isSignedIn && location.pathname !== '/signup') {
+        navigate({ to: '/signin' });
+      } else if (!isSignedIn && location.pathname !== '/signin') {
+        navigate({ to: '/signup' });
+      }
+
+      // if (!isSignedIn) {
+      //   navigate({ to: '/signin' });
+      //   return;
+      // }
+    }
+  }, [isLoaded, isSignedIn]);
 
   return (
     <>
