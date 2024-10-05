ecd6597e4616651e1a287c159000f1092bf165a0
Alex <alex.beciana@gmail.com>
2024-10-05 10:13:27 -0600

    Added navbar options

--- src/components/_navigation/NavBar/index.jsx
+++ src/components/_navigation/NavBar/index.jsx
@@ -1,9 +1,11 @@
+import Image from '../../_styled/Image';
 import UserAccount from '../UserAccount';
+import { Link } from '@tanstack/react-router';
 
 const NavBar = () => {
   return (
-    <nav className="px-10 py-3 flex justify-end items-center">
-      {/* <div>
+    <nav className="px-10 py-3 flex justify-between items-center bg-white">
+      <div>
         <Image
           src="https://media.graphassets.com/output=format:jpg/BZcUI4wrSx6BB1zpOyO6"
           alt="Manulock logo"
@@ -12,7 +14,12 @@ const NavBar = () => {
           priority
           className="h-10 w-20"
         />
-      </div> */}
+      </div>
+      <div>
+        <Link to="/dashboard" className="text-lg">
+          Dashboard
+        </Link>
+      </div>
       <div>
         <UserAccount />
       </div>
