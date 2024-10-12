8ced14f1ae3732c3d022d8f8d7612116b55c18ac
Alex <alex.beciana@gmail.com>
2024-10-12 16:27:21 -0600

    Added layout wrapper

--- src/components/_navigation/NavBar/index.jsx
+++ src/components/_navigation/NavBar/index.jsx
@@ -1,29 +1,32 @@
 import Image from '../../_styled/Image';
 import UserAccount from '../UserAccount';
 import { Link } from '@tanstack/react-router';
+import { DashboardLayout } from '../../_styled/Layout';
 
 const NavBar = () => {
   return (
-    <nav className="px-10 py-3 flex justify-between items-center bg-white">
-      <div>
-        <Image
-          src="https://media.graphassets.com/output=format:jpg/BZcUI4wrSx6BB1zpOyO6"
-          alt="Manulock logo"
-          height={620}
-          width={1060}
-          priority
-          className="h-10 w-20"
-        />
-      </div>
-      <div>
-        <Link to="/dashboard" className="text-lg">
-          Dashboard
-        </Link>
-      </div>
-      <div>
-        <UserAccount />
-      </div>
-    </nav>
+    <DashboardLayout>
+      <nav className="px-10 py-3 flex justify-between items-center bg-white">
+        <div>
+          <Image
+            src="https://media.graphassets.com/output=format:jpg/BZcUI4wrSx6BB1zpOyO6"
+            alt="Manulock logo"
+            height={620}
+            width={1060}
+            priority
+            className="h-10 w-20"
+          />
+        </div>
+        <div>
+          <Link to="/dashboard" className="text-lg">
+            Dashboard
+          </Link>
+        </div>
+        <div>
+          <UserAccount />
+        </div>
+      </nav>
+    </DashboardLayout>
   );
 };
 
