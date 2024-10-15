32985851bc299c37e9ddf1b730eb9f2ec3add20d
Alex <alex.beciana@gmail.com>
2024-10-14 21:33:17 -0600

    Formatted

--- src/components/_styled/Layout/index.jsx
+++ src/components/_styled/Layout/index.jsx
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 
 export const DashboardLayout = ({ children }) => {
   return (
-    <section className="px-10 py-3 max-w-[1440px] mx-auto">{children}</section>
+    <section className="px-5 sm:px-10 py-3 max-w-[1440px] mx-auto">
+      {children}
+    </section>
   );
 };
 
